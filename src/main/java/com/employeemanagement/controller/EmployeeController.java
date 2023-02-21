6548b02448f203ccdcf41866c48bc834f99f2996
ViswaSneha <viswanadh.kasi44@gmail.com>
2023-02-22 00:07:42 +0530

    Employee Management cleaning the code

--- src/main/java/com/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/employeemanagement/controller/EmployeeController.java
@@ -3,7 +3,15 @@ package com.employeemanagement.controller;
 import com.employeemanagement.entity.Employee;
 import com.employeemanagement.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,7 +32,7 @@ public class EmployeeController {
 
         List<Employee> employeeList= employeeService.getEmployees();
 
-        List<Employee> employeeFilterList=employeeList.stream().filter(t->t.getEmpDept().equals("SoftwareEngineer")).collect(Collectors.toList());
+        List<Employee> employeeFilterList=employeeList.stream().filter(t->t.getEmpSal()>1000000).collect(Collectors.toList());
 
         return employeeFilterList;
     }
--- src/main/java/com/employeemanagement/entity/Employee.java
+++ src/main/java/com/employeemanagement/entity/Employee.java
@@ -1,6 +1,12 @@
 package com.employeemanagement.entity;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
 import lombok.Data;
 
 import java.util.Date;
